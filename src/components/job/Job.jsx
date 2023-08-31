08f6c36da1c44bc41084710ff87d713b81ae6a9f
Joshua Chinwendu <joshuachinwendu@gmail.com>
2023-08-31 13:47:33 +0100

    keep testing

--- src/components/job/Job.jsx
+++ src/components/job/Job.jsx
@@ -27,50 +27,16 @@ const Job = () => {
     getJobsData();
   }, []);
 
+  const arr = jobs.map((jobs)=>{
+    return (
+      <p key={jobs.id}>{jobs.title}</p>
+    )
+  })
+
   return (
     <>
       {
-
-        jobs.map(job => {
-
-          return <div className="col-md-4 " key={job.id}>
-            <div className="card p-3 mb-2 jobCard1">
-              <div className="d-flex justify-content-between">
-                <div className="d-flex flex-row align-items-center">
-                  <div className="ms-2 c-details">
-                    <h6 className="mb-0">{job.location}</h6>
-                    <span>
-                      <TimeAgo
-                        datetime={job.createdAt}
-                        locale='vi'
-                      />
-                    </span>
-                  </div>
-                </div>
-                <div className="">
-                  <LinkContainer to={`jobs/${job.id}`} style={{ textDecoration: "none", backgroundColor: "#EF7F1A", color: "white" }}>
-                    <button className="btn">Apply</button>
-                  </LinkContainer>
-                </div>
-              </div>
-              <div className="mt-5">
-                <h3 className="heading">
-                  {job.title}
-                  <br />
-                  <span className="jobComName">
-                  </span>
-                </h3>
-                <div className="mt-5">
-                  <div className="mt-3">
-                    <span className="jobText2 ellipse">
-                      {job.description}
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        })
+arr
       }
 
     </>
